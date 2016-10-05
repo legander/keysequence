c2b8ccbae9ae8575ed58c9eb57338145c06ae263
Simon Legander <simon.legander@gmail.com>
2016-10-05 12:53:23 +0200

    add files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # keysequence
+
+#### usage example
+```
+var mySequence = keySequence('asd', 300, () => {
+  console.log('asd');
+});
+```
+#### removal example
+```
+mySequence.remove();
+```
---
+++ keysequence.js
@@ -0,0 +1,47 @@
+/**
+ * invokes fn when passed key sequence is typed within passed threshold.
+ *
+ * @param {string} sequence of characters such as `qwerty`.
+ * @param {number} threshold between key presses in milliseconds.
+ * @param {function} function to be invoked when sequence is typed successfully.
+ * @return {object} object containing method to remove eventlistener.
+ */
+function keySequence(keySequence, threshold, fn) {
+  'use strict'
+
+  var keyspressed = [];
+  var deadline = null;
+
+  var reset = function() {
+    console.log('reset');
+    keyspressed = [];
+  }
+
+  var onPress = function(e) {
+    if (deadline) {
+      clearTimeout(deadline);
+    }
+    deadline = setTimeout(reset, threshold);
+
+    if (keyspressed.length === keySequence.length) {
+      reset();
+    }
+
+    var keyCode = e.keyCode || e.charCode;
+    var character = String.fromCharCode(keyCode);
+
+    keyspressed.push(character);
+    console.log(keyspressed.join(''), keySequence)
+    if (keyspressed.join('') === keySequence) {
+      fn();
+    }
+  }
+
+  document.addEventListener('keypress', onPress, false);
+
+  return {
+    remove: function() {
+      document.removeEventListener('keypress', onPress);
+    }
+  }
+}
